5870f678c24526b130cb4d12db97020bd7b79496
Richard E. Turner <richard.e.turner@gmail.com>
2012-01-18 20:27:49 +0000

    Neatened up the help.

--- mPAD/tools/unpackParamsMPAD.m
+++ mPAD/tools/unpackParamsMPAD.m
@@ -1,17 +1,17 @@
 function [G,Lam1,Var1,Len2,Var2,Mu2,vary] = unpackParamsMPAD(Params);
   
-  % [G,Lam1,Var1,Len2,Var2,Mu2,vary] = unpackParamsMPAD(Params);
-  %
-  % Unpacks all of the parameters out of the the structure
-  %
-  % INPUTS
-  % Params = structure including
-  %  G,Lam1,Len2,Var1,Var2,Mu2,vary 
-  %
-  % OUTPUTS
-  % G,Lam1,Len2,Var1,Var2,Mu2,vary
-  %
-  % see test_packParamsMPAD.m for tests
+% [G,Lam1,Var1,Len2,Var2,Mu2,vary] = unpackParamsMPAD(Params);
+%
+% Unpacks all of the parameters out of the the structure
+%
+% INPUTS
+% Params = structure including
+%  G,Lam1,Len2,Var1,Var2,Mu2,vary 
+%
+% OUTPUTS
+% G,Lam1,Len2,Var1,Var2,Mu2,vary
+%
+% see test_packParamsMPAD.m for tests
 
 G = Params.G;
 Lam1 = Params.Lam1;
