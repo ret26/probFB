62282612f2656991991eaa63a998b1ea5898f577
Richard E. Turner <richard.e.turner@gmail.com>
2012-01-20 11:56:20 +0000

    Removed the constant added to the background.

--- mPAD/toolsGP/getGPSESpec.m
+++ mPAD/toolsGP/getGPSESpec.m
@@ -14,11 +14,11 @@ function fftCov = getGPSESpec(len,T);
 % OUTPUTS
 % fftCov = power spectrum of the GP, size [T,1]
 
-  tiny = 1e-6;
     
   omega = 2*pi*[[0:floor(T/2)],[-floor(T/2)+1:1:-1]]'/T;
 
   fftCov = sqrt(2*pi*len^2)*exp(-omega.^2*len.^2/2);
-
-  fftCov = fftCov+tiny;
+  
+%  tiny = 1e-6;
+%  fftCov = fftCov+tiny;
   
