89573e14c3c9c8e39b258c4bab48bedc576de1cb
Richard E. Turner <richard.e.turner@gmail.com>
2012-04-16 09:11:02 +0100

    Tweaking the objective.

--- AR2FB/spectra/tests/test_getSpecAR2Obj.m
+++ AR2FB/spectra/tests/test_getSpecAR2Obj.m
@@ -12,12 +12,16 @@ function testRunWithoutError
 
 N = 100;
 D= 5;
-theta = randn(3*D,1);
+theta = [randn(D,1);log(rand(2*D,1)/2)];
 vary = rand;
 specTar = abs(fft(randn(N,1))).^2;
+limCF = cumsum(rand(D,2)'/4)';
+limDF = cumsum(rand(D,2)'/4)';
+minVar = exp(randn(D,1));
+bet = rand;
 
-[Obj,dObj]=getSpecAR2Obj(theta,vary,specTar);
-Obj=getSpecAR2Obj(theta,vary,specTar);
+[Obj,dObj]=getSpecAR2Obj(theta,vary,bet,specTar,minVar,limCF,limDF);
+Obj=getSpecAR2Obj(theta,vary,bet,specTar,minVar,limCF,limDF);
 
 function testGradientNumerically
 
@@ -26,12 +30,17 @@ function testGradientNumerically
 
 N = 100;
 D= 5;
-theta = randn(3*D,1);
+theta = [randn(D,1);log(rand(2*D,1)/2)];
 vary = rand;
 specTar = abs(fft(randn(N,1))).^2;
 
+limCF = cumsum(rand(D,2)'/4)';
+limDF = cumsum(rand(D,2)'/4)';
+minVar = exp(randn(D,1));
+bet = rand;
+
 delta =1e-6;
-vals = checkgrad('getSpecAR2Obj',theta,delta,vary,specTar);
+vals = checkgrad('getSpecAR2Obj',theta,delta,vary,bet,specTar,minVar,limCF,limDF);
 
 tol = 1e-6;
 assertElementsAlmostEqual(vals,0,'absolute',tol,0)
