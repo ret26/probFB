0d265c34165d67d396e1a7fa019acd556f0321d4
Richard E. Turner <richard.e.turner@gmail.com>
2012-04-16 09:22:49 +0100

    Old test of the function which maps from centre-frequency and bandwidth back to the AR(2) parameters.

---
+++ AR2FB/spectra/tests/test_freq2AR2Old.m
@@ -0,0 +1,22 @@
+
+function test_suite = test_freq2AR2Old
+  initTestSuite;
+
+  % tests both freq2AR2 and AR22freq
+  
+function test_consistentWith_AR22freq
+
+dispFigs = 0;
+
+fmax = [1/8,1/4]';
+df = [1/32,1/80]';
+mVar = [1,2]';
+
+[lamx,varx,dft] = freq2AR2Old(fmax,df,mVar,1);
+[fmax2,df2, mVar2] = AR22freq(lamx,varx);
+
+tol = 1e-5;
+assertVectorsAlmostEqual(fmax,fmax2,'absolute',tol,0)
+assertVectorsAlmostEqual(df,df2,'absolute',tol,0)
+assertVectorsAlmostEqual(mVar,mVar2,'absolute',tol,0)
+
