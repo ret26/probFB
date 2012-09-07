90dd7f0345d6e5cf77f13693836eb9bdcde72872
Richard E. Turner <richard.e.turner@gmail.com>
2012-09-07 10:13:02 +0100

    Improved help to make it more descriptive.

--- probSpec/probFB.m
+++ probSpec/probFB.m
@@ -5,12 +5,14 @@ function [Z,varargout] = probFB(y,lamx,varx,om,vary,varargin)
   % Probabilistic Filter Bank (see Turner 2010, Chapter 5 for details)
   %
   % In the standard mode above the FFT is used to compute the AR
-  % filter bank. This is fast.
+  % filter bank. This is fast. When the optional inputs/output below
+  % are added/requested, the kalman mode is used which is slower.
   % 
   % NOTE: THAT THE FFT METHOD GIVES A SLIGHTLY DIFFERENT SOLUTION
   % FROM THE KALMAN BASED METHODS (SEE BELOW). SPECIFICALLY, AT THE
   % START AND END OF THE SIGNAL THERE ARE DISCREPANCIES DUE TO THE
-  % CIRCULAR BOUNDARY CONDITIONS ASSUMED.
+  % CIRCULAR BOUNDARY CONDITIONS ASSUMED. IN GENERAL THOUGH, THE
+  % TWO METHODS WILL BE EXTREMELY SIMILAR.
   %
   % With optional inputs and outputs:
   % function [Z,covZ] = probSTFT(y,lamx,varx,om,vary,verbose,KF)
@@ -56,7 +58,7 @@ if KF==0 & nargout==1 & length(vary)==1
 else
   
   % Kalman Smoothing
-  [lik,Xfin,Pfin] = kalmanSlowFB(lamx,varx,om, vary,y,verbose,KF);
+  [lik,Xfin,Pfin] = kalmanSlowFB(lamx,varx,om,vary,y,verbose,KF);
 
   % Output
   [Z,covZ] = getFBLDSOutput(Xfin,Pfin);
