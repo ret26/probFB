c6c67da8343ece4c80e30f2cb8cc895eb9450a91
R.E. Turner <ret26@annapurna.eng.cam.ac.uk>
2012-09-11 12:02:17 +0100

    Fits a probabilistic STFT model to a signal.
    Works adequately at the moment, but needs work yet to determine whether it can be improved.
    This can be tested using the held out likelihood calculated in the testing function.

---
+++ probSpec/fit_probSTFT.m
@@ -0,0 +1,256 @@
+function [varx,lamx,om,Info] = fit_probSTFT(y,D,varargin)
+
+% function [varx,lamx,om,Info] = fit_probSTFT(y,D,Opts)
+% Optional inputs:
+% function [varx,lamx,om,Info] = fit_probSTFT(y,D,Opts)
+%
+% Fits a probabilstic STFT to a signal y.
+% x_{1,t,d} = lam_d x_{1,t-1,d} +\eta_{1,t,d} \varx_d^{1/2}
+% x_{2,t,d} = lam_d x_{2,t-1,d} +\eta_{2,t,d} \varx_d^{1/2}
+% \eta_{i,t,d} ~ \Norm(0,1)
+% y_t = real(\sum_{d} exp(i om_d)*(x_{1,t,d}+i x_{2,t,d}))
+%
+% Fits the parameters (lam_d, varx_d,om_d) such that the spectra are
+% matched to the target spectrum specified in specTar.
+%
+% Specifically, we integrate out all of the hidden variables which
+% renders the time-domain fitting problem into a spectrum matching
+% problem. The log-likelihood being,
+%
+% like(\theta) = -1/2 \sum_t [ log(specModel_t(\theta)) +
+%                 specTar_t/specModel_t(\theta) ]
+%
+% As spectra of y (specTar) are noisy (they have high variance) the
+% likelihood has lots of local optima. In order to alleviate this
+% effect we use a coarse to fine procedure based on using Welch's
+% periodogram to estimate the spectrum of the signal y, rather than
+% the standard fft method. This smooths the spectrum, reducing the
+% variance at the cost of introducing some bias, but importantly it
+% prevents local optima in the likelihood. We reduce the degree of
+% smoothing through the optimisation (i.e. this is a coarse to fine
+% method). 
+%
+% i.e. we define
+% specTar_{t,n} = a matrix containing the spectra of sections of y, then
+% mn_specTar_t = 1/N * \sum_n specTar_{t,n}
+% we covert this to a power spectral density estimate by dividing
+% by the frequency resolution.
+% We then fit the mean spectrum to mn_specTar.
+%
+% An alternative theoretical perspective is that we split the signal
+% into a bunch of short chunks. We then model each as an independent
+% draw from a probabilistic spectrogram. The likelihood is therefore a sum
+% of the likelihood's derived above:
+%
+% like(\theta) = -1/2 \sum_{t,n} [ log(specModel_t(\theta)) +
+%                 specTar_{t,n}/specModel_t(\theta) ]
+%
+% In order to do the fitting 
+
+% The function parameterises the AR processes using the marginal
+% variance of the processes rather than the conditional (marginal
+% variance = conditional variance/(1-lam^2)) since the objective is
+% more simply behaved in this space and the constraints on the
+% parameters are simpler to enforce.
+%
+% See Chapter 5 of my thesis (Statistical Models for Natural Sounds
+% by R.E.Turner) for more details about AR(2) Filter banks.
+%
+%
+% INPUTS
+% y = the signal, size [T,1]
+% D = number of AR(2) processes to fit 
+% OPTIONAL INPUTS:
+% Opts = set of options
+% 
+% OUTPUTS
+% varx = innovations variance, size [D,1] (D = number of processes)
+% om = centre frequencies, size [D,1]
+% lam = dynamical parameters, size [D,1] 
+% Info = structure containing information about the estimation
+%        process including
+%        Objs = objectives at each scale concatenated into a vector
+%        ins = number of completed iterations at each scale
+
+
+% Rescale and zero-mean y (we rescale the variances at the end to
+% compensate)
+y = y(:) - mean(y(:));
+varSig = var(y);
+y = y/sqrt(varSig);
+
+% Initialise the STFT parameters
+% uniformly over log-frequency and make them fairly broad
+mVar = ones(D,1)/D;
+FLim = [1/50,0.3]; % limits for the centre frequency initialisation
+dfFrac = 1/5; % width of the processes wrt centre-frequency
+fmax = logspace(log10(FLim(1)),log10(FLim(2)),D)';
+[om,lamx] = freq2probSpec(fmax,fmax*dfFrac,1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COARSE TO FINE PROCESS
+numLevels = 40; % number of levels
+numIts = 50; % number of iterations per level
+minT = 200;%200; % minimum segment size to compute spectrum of
+maxT = 3000; % maximum segment size to compute spectrum of
+
+% Constraints on the variables
+minVar = mVar/400; % minimum marginal variance - might want to taper
+omLim = ones(D,1)*[0,pi]; % limits on the centre frequency
+small = 1e-5; % limits on the lambda parameter - might want to
+              % taper this value so things don't get narrow too
+              % early
+lamLim = ones(D,1)*[0,1-small];
+
+% Observation noise tapered
+vary_an = logspace(log10(1e-6),log10(1e-10),numLevels);
+		 
+bet = 100; % how strongly to encourage variances to be pruned
+		 
+T = length(y);
+
+numFreq = floor(logspace(log10(min([minT,T])),log10(min([maxT,T])),numLevels));
+ovLp = floor(numFreq/10);
+
+Objs = [];
+
+% keep the historical values of the variables as they evolve
+omHist = repmat(NaN,[D,numLevels+1]);
+lamHist = repmat(NaN,[D,numLevels+1]);
+mVarHist = repmat(NaN,[D,numLevels+1]);
+
+omHist(:,1) = om;
+lamHist(:,1) = lamx;
+mVarHist(:,1) = mVar;
+
+% Compute the Held Out likelihood if the user has requested it
+if nargin>2
+  if isfield(varargin{1},'yHO')
+    compHOLike = 1;
+    yHO = varargin{1}.yHO;
+    yHO = yHO(:)/sqrt(varSig);
+    
+    THO = length(yHO);
+    likeHO = repmat(NaN,[numLevels,1]);
+    [pgHO,varpg] = welchMethod(yHO,THO,0);
+    pgHO = pgHO/(1/2/THO);
+  
+    if mod(THO,2)==0
+    % if even
+    specHO = [pgHO;pgHO(end-1:-1:2)];
+    else
+    % if odd
+    specHO = [pgHO;pgHO(end:-1:2)];
+    end
+  else
+    compHOLike = 0;
+  end
+else
+  compHOLike = 0;
+end
+
+
+for c2f=1:numLevels
+
+  % Estmate spectrum of y using Welch's periodogram
+  [pg,varpg] = welchMethod(y,numFreq(c2f),ovLp(c2f));
+  pg = pg/(1/2/numFreq(c2f)); % convert to power spectral density estimate
+
+  %pg = pg*numFreq;
+
+  if mod(T,2)==0
+    % if even
+    specTar = [pg;pg(end-1:-1:2)];
+  else
+    % if odd
+    specTar = [pg;pg(end:-1:2)];
+  end  
+
+  % Constraints on variables
+ 
+  % current settings of the variables
+  theta = [log(mVar-minVar);...
+	   log(om-omLim(:,1))-log(omLim(:,2)-om);...
+	   log(lamx-lamLim(:,1))-log(lamLim(:,2)-lamx)]; 
+  % Base line noise to avoid divide by zero problems
+  vary = max(specTar)*vary_an(c2f);
+  
+  % Fit the spectrum using probabilistc spectrogram
+  [theta,ObjCur,inCur] = minimize(theta,'get_Obj_pSTFT_spec',numIts, ...
+				  vary, specTar,minVar,omLim, ...
+				  lamLim,bet*numFreq(c2f)/numFreq(c2f(1)));
+
+
+  if compHOLike==1
+    % Compute HO likelihood if asked for
+    [likeHO(c2f,1),dObjTemp] = get_Obj_pSTFT_spec(theta,vary,specHO,minVar, ...
+						   omLim,lamLim,0);
+  
+  end
+  
+  % Collect information about the current iteration
+  Objs = [Objs;ObjCur];
+  ins(c2f) = inCur;
+  
+  % Compute the current settings of the parameters
+  mVar= minVar+exp(theta(1:D));
+  om = omLim(:,1)+(omLim(:,2)-omLim(:,1))./(1+exp(-theta(D+1:2*D)));
+  lamx = lamLim(:,1)+(lamLim(:,2)-lamLim(:,1))./(1+exp(-theta(2*D+1:3*D)));
+
+    
+  % Store historical values for the processes
+  omHist(:,c2f+1) = om;
+  lamHist(:,c2f+1) = lamx;
+  mVarHist(:,c2f+1) = mVar;
+
+  % plot if requested by user
+  if nargin>2
+    if isfield(varargin{1},'verbose')
+      if varargin{1}.verbose==1
+	% plot the fit
+	if compHOLike==1
+	  figH =  plot_fit_probSTFT(pg,mVar,om,lamx,mVarHist,omHist,lamHist, ...
+				    Objs,ObjCur,minVar,likeHO);  
+	else
+	  figH =  plot_fit_probSTFT(pg,mVar,om,lamx,mVarHist,omHist,lamHist, ...
+				    Objs,ObjCur,minVar);  
+	end
+      end
+    end
+  end
+
+% re-initialised the pruned components of the model - doing
+% something dumb ATM
+freq = linspace(0,1/2,length(pg));
+       
+for d=1:D    
+  if mVar(d)/minVar(d)<10
+    mVar(d) = 1/5;
+    om(d) = rand*pi;
+    lam(d) = 0.3;
+  end
+end
+
+  
+  % for n=1:
+
+  %     theta = [log(mVar-minVar);...
+  % 	   log(cosCF-cosCFLim(:,1))-log(cosCFLim(:,2)-cosCF);...
+  % 	   log(cosDF-cosDFLim(:,1))-log(cosDFLim(:,2)-cosDF)]; 
+  %     [likeHO(c2f,1),dObjTemp] = getSpecAR2Obj(theta,vary,0,specTar,minVar,cosCFLim,cosDFLim);
+  % end
+
+end
+
+Info.Objs = Objs;
+Info.ins = ins;
+
+% Rescale the variance parameters so their sum is equal to that of
+% the input signal
+rescale = varSig/sum(mVar);
+varx = mVar.*(1-lamx.^2);
+varx = rescale*varx;
+
+if compHOLike==1
+  Info.likeHO=likeHO;
+end
---
+++ probSpec/tests/test_fit_probSTFT.m
@@ -0,0 +1,145 @@
+function test_suite = test_fit_probSTFT
+  initTestSuite;
+
+% Tests:
+%
+% function [Lam,Var,Info] = fitAR2FB(y,D,varargin)
+% 
+  
+function test_pSTFT
+
+% Tests the function runs on data generated from a probabilistic spectrum
+
+dispFigs = 1;
+T = 10000;
+
+% D = 3;
+% CF = [1/32;1/20;1/10];
+% DF = [1/128;1/50;1/100];
+% varMa = [1;4;2];
+
+D = 4;
+fmaxTr = [1/32;1/20;1/10;1/15];
+dfTr = [1/128;1/100;1/100;1/500];
+varMaTr = [1;4;2;2];
+
+vary = 0;
+
+[omTr,lamxTr,varxTr] = freq2probSpec(fmaxTr,dfTr,varMaTr);
+
+[y,X] = samplePSTFT(lamxTr,varxTr,omTr,vary,T);
+[yHO,X] = samplePSTFT(lamxTr,varxTr,omTr,vary,T);
+
+opts.verbose = 1;
+opts.yHO = yHO;
+
+[varxEst,lamxEst,omEst,Info] = fit_probSTFT(y,D,opts);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+specSig = abs(fft(y)).^2;
+freqSig = linspace(0,1/2,floor(T/2));
+
+specTr = zeros(D,T);
+specEst = zeros(D,T);
+
+freqs = linspace(0,1/2,T);
+
+specTr = get_pSTFT_spec(freqs,lamxTr,varxTr,omTr);
+specEst = get_pSTFT_spec(freqs,lamxEst,varxEst,omEst);
+  
+cumsumSig = 2*cumsum(specSig(1:floor(T/2))/T);
+cumsumTr = cumsum(sum(specTr,1));
+cumsumEst = cumsum(sum(specEst,1));
+
+if dispFigs==1
+
+  figure
+  subplot(2,1,1)
+  hold on
+
+  ah1=plot(freqSig,specSig(1:floor(T/2))/T,'-k');
+  for d=1:D
+    ah2=plot(freqs,specTr(d,:),'-g');
+    ah3=plot(freqs,specEst(d,:),'-m');
+  end
+  legend([ah1,ah2,ah3],'data','true','estimated')
+
+  subplot(2,1,2)
+  hold on
+  ah1=plot(freqSig,cumsumSig,'-k');
+  ah2=plot(freqs,cumsumTr,'-g');
+  ah3=plot(freqs,cumsumEst,'-m');
+  legend([ah1,ah2,ah3],'data','true','estimated')
+end
+
+tol = 5e-1;
+assertElementsAlmostEqual(100+cumsumEst(1:2:end)',100+cumsumSig,'relative',tol,0)
+
+
+function testSpeech
+
+% Tests the function runs on speech - run using:
+% $ runtests test_fitAR2FB:testSpeech
+dispFigs=1;
+File = '74 - Sentences.wav';
+% % %File = '87 - Wind and Leaves.wav';
+% %File = '80 - Fire.wav';
+% % %File = '05 - Bubbling stream.wav';
+% %File = '07 - Bullfrogs.wav';
+% %File = '06 - Budgerigars.wav';
+% %File = '24 - Fire Burning.wav';
+% % %File = '81 - Wind.wav';
+% % File = '82 - Rain.wav';
+
+soundPath = '~/Music/Various Artists/Best Ever Sound Effects - Vol.3 - Sounds Of Nature Sound Effects/';%soundPath = '~/probFB/demos/signals/';
+[yOrig,FSamp] = wavread([soundPath,File]);
+RngLim = [FSamp*0.2+1,4.2*FSamp];
+y = yOrig(RngLim(1):RngLim(2));
+y = y - mean(y);
+y = y/sqrt(var(y));
+yHO = yOrig(RngLim(2)+(RngLim(1):RngLim(2)));
+yHO = yHO/sqrt(var(yHO));
+
+D = 15;
+
+opts.verbose = 1;
+opts.yHO = yHO;
+
+[varxEst,lamxEst,omEst,Info] = fit_probSTFT(y,D,opts);
+
+Info.likeHO(end)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+T = length(y);
+specSig = abs(fft(y)).^2;
+freqSig = linspace(0,1/2,floor(T/2));
+
+freqs = linspace(0,1/2,T);
+
+specEst = get_pSTFT_spec(freqs,lamxEst,varxEst,omEst);
+
+cumsumSig = 2*cumsum(specSig(1:floor(T/2))/T);
+cumsumEst = cumsum(sum(specEst,1));
+
+if dispFigs==1
+
+  figure
+  subplot(2,1,1)
+  hold on
+
+  ah1=plot(freqSig,specSig(1:floor(T/2))/T,'-k');
+  for d=1:D
+    ah3=plot(freqs,specEst(d,:),'-m');
+  end
+  legend([ah1,ah3],'data','estimated')
+  set(gca,'yscale','log')
+  
+  subplot(2,1,2)
+  hold on
+  ah1=plot(freqSig,cumsumSig,'-k');
+  ah3=plot(freqs,cumsumEst,'-m');
+  legend([ah1,ah3],'data','estimated')
+end
+
+tol = 5e-1;
+assertElementsAlmostEqual(100+cumsumEst(1:2:end)',100+cumsumSig,'relative',tol,0)
