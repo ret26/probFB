ae3a075fb9031ad1873a9d58e020cd7d373e5b89
Richard E. Turner <richard.e.turner@gmail.com>
2012-10-15 13:23:57 +0100

    Extended to use either AR(2) or pFB carriers.

--- mPAD/genModel/sampleMPAD.m
+++ mPAD/genModel/sampleMPAD.m
@@ -2,19 +2,50 @@ function [Y,X1,X2,A] = sampleMPAD(Params,Dims)
 
   % function [Y,X1,X2,A] = sampleMPAD(Params,Dims)
   %
-  % Samples T time-steps from the multivariate
-  % probabilistic amplitude demodulation. See Turner 2010 Chapter 5
-  % for details of the model.
+  % Samples T time-steps from the multivariate probabilistic amplitude
+  % demodulation. See Turner 2010 Chapter 5 for details of the model.
   %
+  % The model either uses carriers which are of the AR(2) type or
+  % of the probabilistic filter bank type.
+  %
+  % y_t = \sum_{d=1}^D c_{d,t} a_{d,t} + \sig_y \epsilon_t
+  % \epsilon_t ~ \Norm(0,1)
+  %
+  % Here c_{d,t} are a set of narrow band carrier which are either
+  % second order auto-regressive processes (AR(2)) or probabilistic
+  % filter bank processes (pFB): 
+  %
+  % AR(2)
+  % c_{d,t} = \lambda_{1,d} c_{d,t-1} +  ...
+  %                    \lambda_{2,d} c_{d,t-2} +\sig_d \eta_{d,t}
+  % \eta ~ \Norm(0,1)
+  %
+  % pFB
+  % c_{d,t} = real(z_{d,t})
+  % z_{d,t} = \lambda_d \exp(i \omega_d) z_{d,t-1} + ...
+  %                     \sig_d (\eta_{d,t}+i\eta_{d,t}
+  %
+  % The a_{d,t} are a set of positive amplitudes or modulators:
+  % a_{d,t} = \log(1+exp(\sum_{k} g_{d,k}  x2_{t,k} + \mu_{d}))
+  %
+  % The 'transformed envelopes' x2_{t,k} are drawn from Gaussian processes:
+  % x2_{1:T,k} ~ \Norm(0,cov_k(t,t'))
+  % The covariance function of these Gaussian processes is:
+  % cov_k(t,t') = exp(-(t-t')^2/2 len_d^2)
+  % where the characteristic time-scale of the Gaussian processes
+  % is given by len_d
+  %
+  % 
   % INPUTS  
   % Params = structure containing
   %   G = Generative weights [D,K] 
-  %   Lam1 = Carrier dynamics [D,2]
+  %   Lam1 = Carrier dynamics [D,2] or optionally for pFB carriers [D,1]
   %   Var1 = Carrier noise [1,D]
   %   Len2 = Modulator Length-Scales [K,1]
   %   Var2 = Modulator Noise [1,K]
   %   Mu2 = Modulator means [K,1]
   %   vary = observation noise  
+  %   optionally for pFB carriers: om = centre frequencies [D,1]
   %
   % Dims = Structure containing all the dimensionalities
   % D = number of carriers
@@ -23,15 +54,34 @@ function [Y,X1,X2,A] = sampleMPAD(Params,Dims)
   %
   % OUTPUTS
   % Y = Observations [T,1]
-  % X1 = carriers [T,D]
+  % X1 = carriers [T,D] (complex valued for pFB)
   % X2 = modulators [T,K]
   % A = amplitudes [T,D]  
     
-[G,Lam1,Var1,Len2,Var2,Mu2,vary] = unpackParamsMPAD(Params);
-[D,K,T] = unpackDimsMPAD(Dims);
+  
 
-% Generate the carriers
-[temp,X1] = sampleAR2FB(Lam1,Var1,0,T);
+  [D,K,T] = unpackDimsMPAD(Dims);
+  
+  % Figure out the carrier type
+  if min(size(Params.Lam1))==1
+    pFB =1; 
+  else
+    pFB =0;
+  end
+
+  % Read out the parameters and sample the carriers
+  if pFB==1
+    [G,Lam1,Var1,Len2,Var2,Mu2,vary,om] = unpackParamsMPAD(Params);
+    
+    % Generate the carriers
+    [temp,X1] = samplePFB(Lam1,Var1,om,0,T);
+  else
+    [G,Lam1,Var1,Len2,Var2,Mu2,vary] = unpackParamsMPAD(Params);
+    
+    % Generate the carriers
+    [temp,X1] = sampleAR2FB(Lam1,Var1,0,T);
+  end
+    
 
 % Generate the transformed amplitudes
 X2 = repmat(NaN,[T,K]);
@@ -48,4 +98,4 @@ end
 A = log(1+exp(X2*G'+ones(T,1)*Mu2'));
   
 % Generate the observations
-Y = sum(A.*X1,2) + randn(T,1)*sqrt(vary);
+Y = sum(A.*real(X1),2) + randn(T,1)*sqrt(vary);
--- mPAD/genModel/tests/test_sampleMPAD.m
+++ mPAD/genModel/tests/test_sampleMPAD.m
@@ -61,7 +61,7 @@ assertVectorsAlmostEqual(autoCor1,autoCorEmp1','absolute',tol,0)
 assertVectorsAlmostEqual(autoCor2,autoCorEmp2','absolute',tol,0)
 
 
-function test_correctAutoCorrelationCars
+function test_correctAutoCorrelationCars_AR2
 
 dispFigs = 1;
 randn('state',1)
@@ -97,3 +97,102 @@ autoCor2B = [var(X1(:,2)); mean(X1(2:T,2).*X1(1:T-1,2))];
 tol =3e-1;
 assertVectorsAlmostEqual(autoCor1A,autoCor1B,'absolute',tol,0)
 assertVectorsAlmostEqual(autoCor2A,autoCor2B,'absolute',tol,0)
+
+
+
+function test_correctAutoCorrelationCars_pFB
+
+dispFigs = 1;
+randn('state',1)
+vars = [1.5,0.7];
+lens = [5.32,3.4]';
+tau = 20;
+T = 100000;
+
+Lam1  = [0.9,0.7]';
+Var1  = (1-Lam1.^2);
+Om = [pi/10,pi/20]';
+
+
+Dims.D = 2;
+Dims.K = 2;
+Dims.T = T;
+
+Params.G = randn(2);
+Params.Len2 = lens;
+Params.Var2 = vars;
+Params.Mu2 = randn(2,1);
+Params.Var1 = Var1;
+Params.Lam1 = Lam1;
+Params.vary = 0.1;
+Params.om = Om;
+
+[Y,Za,X2,A] = sampleMPAD(Params,Dims);
+
+X1 = real(Za);
+tau = 50;
+
+autoCor1A = zeros(tau,1);
+autoCor2A = zeros(tau,1);
+
+for t=1:tau
+  autoCor1A(t) = mean(X1(t:T,1).*X1(1:T-t+1,1));
+  autoCor2A(t) = mean(X1(t:T,2).*X1(1:T-t+1,2));
+end
+
+X2 = imag(Za);
+
+autoCor1B = zeros(tau,1);
+autoCor2B = zeros(tau,1);
+
+for t=1:tau
+  autoCor1B(t) = mean(X2(t:T,1).*X2(1:T-t+1,1));
+  autoCor2B(t) = mean(X2(t:T,2).*X2(1:T-t+1,2));
+end
+
+[Yb,Zb] = samplePFB(Lam1,Var1,Om,0,T);
+
+autoCor1C = zeros(tau,1);
+autoCor2C = zeros(tau,1);
+
+for t=1:tau
+  autoCor1C(t) = mean(X1(t:T,1).*X1(1:T-t+1,1));
+  autoCor2C(t) = mean(X1(t:T,2).*X1(1:T-t+1,2));
+end
+
+
+X2 = imag(Zb);
+
+autoCor1D = zeros(tau,1);
+autoCor2D = zeros(tau,1);
+
+for t=1:tau
+  autoCor1D(t) = mean(X2(t:T,1).*X2(1:T-t+1,1));
+  autoCor2D(t) = mean(X2(t:T,2).*X2(1:T-t+1,2));
+end
+
+
+if dispFigs==1
+  figure
+  subplot(2,1,1)
+  hold on
+  plot(autoCor1A,'-k','linewidth',2)
+  plot(autoCor1B,'-r')
+  plot(autoCor1C,'-c','linewidth',1/2)
+  plot(autoCor1D,'-y','linewidth',1/2)
+ 
+  subplot(2,1,2)
+  hold on
+  plot(autoCor2A,'-k','linewidth',2)
+  plot(autoCor2B,'-r')
+  plot(autoCor2C,'-c','linewidth',1/2)
+  plot(autoCor2D,'-y','linewidth',1/2)
+
+end
+
+tol =3e-1;
+assertVectorsAlmostEqual(autoCor1A,autoCor1B,'absolute',tol,0)
+assertVectorsAlmostEqual(autoCor1A,autoCor1C,'absolute',tol,0)
+assertVectorsAlmostEqual(autoCor2A,autoCor2B,'absolute',tol,0)
+assertVectorsAlmostEqual(autoCor2A,autoCor2C,'absolute',tol,0)
+
