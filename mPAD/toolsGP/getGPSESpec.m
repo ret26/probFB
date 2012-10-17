b025dd1f8ab847e9243889440e1d7d0bd868e5d0
Richard E. Turner <richard.e.turner@gmail.com>
2012-10-17 18:05:55 +0100

    Extended to hand many covariances at once.

--- mPAD/toolsGP/getGPSESpec.m
+++ mPAD/toolsGP/getGPSESpec.m
@@ -8,17 +8,22 @@ function fftCov = getGPSESpec(len,T);
 % <x(t)x(t')> = exp(-1/(2*len^2)*(t-t')^2)
 %
 % INPUTS
-% len = length scale of the GP
+% len = length scale of the GP, [D,1]
 % T = number of frequencies over which to evaluate the spectrum
 %
 % OUTPUTS
-% fftCov = power spectrum of the GP, size [T,1]
+% fftCov = power spectrum of the GP, size [T,D]
 
     
   omega = 2*pi*[[0:floor(T/2)],[-floor(T/2)+1:1:-1]]'/T;
 
-  fftCov = sqrt(2*pi*len^2)*exp(-omega.^2*len.^2/2);
+  D = length(len);
+  fftCov = zeros(T,D);
   
-%  tiny = 1e-6;
+  for d=1:D
+    fftCov(:,d) = sqrt(2*pi*len(d)^2)*exp(-omega.^2*len(d).^2/2);
+  end
+
+  %  tiny = 1e-6;
 %  fftCov = fftCov+tiny;
   
--- mPAD/toolsGP/tests/test_getGPSESpec.m
+++ mPAD/toolsGP/tests/test_getGPSESpec.m
@@ -17,7 +17,7 @@ fftCov = getGPSESpec(len,T);
 
 autoCor1 = var*ifft(fftCov);
 
-dts = [0:T/2,[T/2-1:-1:1]];
+dts = [0:T/2,[T/2-1:-1:1]]';
 
 autoCor2 = var*exp(-1/(2*len^2)*(dts).^2);
 
@@ -30,4 +30,44 @@ if dispFigs==1
 end
 
 tol = 1e-5;
-assertVectorsAlmostEqual(autoCor1,autoCor1,'absolute',tol,0)
+assertVectorsAlmostEqual(autoCor1,autoCor2,'absolute',tol,0)
+
+
+function test_correctAutoCorrelation_2D
+
+dispFigs=1;
+randn('state',1)
+var = 1.5;
+len = [5.32;4.12];
+
+T = 1000;
+
+fftCov = getGPSESpec(len,T);
+
+autoCor1A = var*ifft(fftCov(:,1));
+autoCor1B = var*ifft(fftCov(:,2));
+
+dts = [0:T/2,[T/2-1:-1:1]]';
+
+autoCor2A = var*exp(-1/(2*len(1)^2)*(dts).^2);
+autoCor2B = var*exp(-1/(2*len(2)^2)*(dts).^2);
+
+
+if dispFigs==1
+  figure;
+  subplot(2,1,1)
+  hold on
+    
+  plot(autoCor2A,'-k','linewidth',2)
+  plot(autoCor1A,'-r','linewidth',1)
+
+  subplot(2,1,2)
+  hold on
+  plot(autoCor2B,'-k','linewidth',2)
+  plot(autoCor1B,'-r','linewidth',1)
+
+end
+
+tol = 1e-5;
+assertVectorsAlmostEqual(autoCor1A,autoCor2A,'absolute',tol,0)
+assertVectorsAlmostEqual(autoCor1B,autoCor2B,'absolute',tol,0)
