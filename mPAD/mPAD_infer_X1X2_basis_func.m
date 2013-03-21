54104ac582400205c90cd422872df9f73feb62a0
R.E. Turner <ret26@annapurna.eng.cam.ac.uk>
2013-03-21 11:06:53 +0000

    Infer latent variables in mPAD using basis function representation.

---
+++ mPAD/mPAD_infer_X1X2_basis_func.m
@@ -0,0 +1,116 @@
+function [X1,X2,Info] = mPAD_infer_X1X2_basis_func(y,Z2,Params,varargin)
+
+% function [X1,X2,Params,Info] = mPAD_train_G_basis_func(y,Z2,Params,varargin)
+%
+% Infers the modulators and carriers in mPAD. Requires initialised
+% estimates of the envelopes and the parameters.
+%
+% INPUT
+% y = Obsevations [T,1] 
+% Z2 = initial estimates for transformed envelope coefficients [T,K] 
+% Params = structure containing the following parameters
+%   G = modulator weights [D,K]
+%   Lam1 = Carrier dynamics [D,2]
+%   Var1 = Carrier noise [1,D]
+%   Len2 = Modulator Length Scales [K,1] (not required)
+%   Var2 = Modulator Noise [1,K]
+%   vary = observation noise  
+% opts = structure of optional options
+%   numIts = number of iterations
+%   progress_chuck = number of iterations for each conjugate
+%                    gradient run
+%   verbose = if verbose==1 plots graphs each iteration
+%
+% OUTPUT
+% X1 = carriers (complex) [T,1]
+% X2 = Transformed envelopes [Tx,K]
+% Info = structure containing information about the optimisation
+%        Obj = objectives
+%        it = number of completed iterations
+
+if nargin>3 & isfield(varargin{1},'numIts')
+  numIts = varargin{1}.numIts;
+else
+  numIts = 200;
+end
+
+if nargin>3 & isfield(varargin{1},'progress_chunk')
+  progress_chunk = varargin{1}.progress_chunk;
+else
+  progress_chunk = 10;
+end
+
+if nargin>3 & isfield(varargin{1},'verbose')
+  verbose = varargin{1}.verbose;
+else
+  verbose = 0;
+end
+
+[G,Lam1,Var1,Len2,Var2,Mu2,vary,om] = ...
+    unpackParamsMPAD(Params);
+
+[T,K] = size(Z2);
+D = length(Var1);
+
+
+z2 = Z2(:);
+  
+L = ceil(numIts/progress_chunk);
+numIts = ones(L,1)*progress_chunk;
+
+Obj = []; it = []; tim = [];
+
+tol = 7;
+X2 = Z2_to_X2(Z2,Len2,Var2,tol);
+
+for l=1:L
+
+  Gold = G;  
+  X2old = X2;
+
+  tic;
+  [z2, ObjCur, itCur] = minimize(z2,'getObj_mPAD_noG_basis_func', ...
+				 numIts(l),y,Params,tol);
+  timCur = toc;
+
+
+  Z2 = reshape(z2,[T,K]);      
+  X2 = Z2_to_X2(Z2,Len2,Var2,tol);
+    
+  % plot if desired
+  if verbose==1
+    plot_mPAD_train_G(y,X2,X2old,Params)  
+  end
+  
+  % Store objective and iteration information
+  Obj = [Obj;ObjCur];
+  it = [it;itCur];
+  tim = [tim;timCur];
+  dX2 = sqrt(sum((X2(:)-X2old(:)).^2)/sum(X2(:).^2));
+  
+  % Display some information to the user
+  str1 = ['Progress ',num2str(l),'/',num2str(L),];
+  str2 = ['Obj ',num2str(ObjCur(end))];
+  str3 = ['dObj ',num2str(diff(ObjCur(end-1:end)))];
+  str4 = ['time ',num2str(ceil(timCur/6)/10),'mins'];
+  str5 = ['total time ',num2str(ceil(sum(tim)/6)/10),'mins'];
+  str7 = ['dX2 ',num2str(round(dX2*1000)/10),'%%'];
+
+  str_space = '   ';
+  
+  fprintf(['\n',str1,str_space,str2,str_space,str3,str_space,str4,str_space,str5,str_space,str7,str_space])
+
+end
+
+fprintf('\n')
+
+% Save information about the optimisation
+Info.Obj = Obj;
+Info.it = it;
+
+% Estimate X1
+A = log(1+exp(X2(1:T,:)*G'+ones(T,1)*Mu2'));
+[ObjA,Xfin,Pfin] = kalman_mPAD_FB(Params,y,A');
+
+[X1,covX1] = getFBLDSOutput(Xfin,Pfin);
+X1 = X1';
---
+++ mPAD/tests/test_mPAD_infer_X1X2_basis_func.m
@@ -0,0 +1,236 @@
+function test_suite = test_mPAD_infer_X1X2_basis_func
+  initTestSuite;
+
+% Tests: 
+%
+% [X1,X2,Info] = mPAD_infer_X1X2_basis_func(y,Z2,Params,varargin)
+%
+
+function test_runs
+
+% quick test to make sure any modifications haven't broken the code
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Forward model Settings
+
+seed = 1; 
+randn('state',seed)
+rand('state',seed)
+
+T = 20;
+FSamp = 8000;
+D = 3;
+K = 2;
+Dims = packDimsMPAD(D,K,T);
+
+% Carrier Processes
+mVar1 = ones(1,D);
+CF = [400,750,1000]/FSamp;
+DF = [40,50,80]/FSamp;
+
+% Modulator Processes
+Len2 = [5,10]; % time constant in samples of modulators
+Mu2 = randn(D,1);
+Var2 = 9*ones(K,1);
+
+G = [1,0;
+     0,1;
+     1,1];
+
+vary = 0.001;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GENERATE DATA
+
+[om,Lam1,Var1] = freq2probSpec(CF,DF,mVar1);
+
+Params = packParamsMPAD(G,Lam1,Var1,Len2,Var2,Mu2,vary,om);
+[y,X1,X2,Amp] = sampleMPAD(Params,Dims);
+
+opts.numIts = 6;
+opts.progress_chunk = 3;
+[X1,X2,Info] = mPAD_infer_X1X2_basis_func(y,X2,Params,opts);
+
+
+% function test_initialised_at_true_X2_stay_put
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % Forward model Settings
+% dispFigs=1;
+
+% opts.verbose = dispFigs;
+% seed = 1; 
+% randn('state',seed)
+% rand('state',seed)
+
+% T = 1000;
+% FSamp = 8000;
+% D = 3;
+% K = 2;
+% Dims = packDimsMPAD(D,K,T);
+
+% % Carrier Processes
+% mVar1 = ones(1,D);
+% CF = [600,750,1000]/FSamp;
+% DF = [60,100,200]/FSamp;
+
+% % Modulator Processes
+% Len2 = [20,20]; % time constant in samples of modulators
+% Mu2 = -ones(D,1);
+% Var2 = 9*ones(K,1);
+
+% G = [1,0;
+%      0,1;
+%      1,1];
+
+% vary = 0.001;
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % GENERATE DATA
+
+% [om,Lam1,Var1] = freq2probSpec(CF,DF,mVar1);
+
+% Params = packParamsMPAD(G,Lam1,Var1,Len2,Var2,Mu2,vary,om);
+% [y,X1,X2,Amp] = sampleMPAD(Params,Dims);
+
+% if dispFigs==1
+
+%   figure
+%   for d=1:D
+%     subplot(D,1,d)
+%     hold on
+%     Ychan = real(X1).*Amp;
+%     plot(Ychan(:,d),'-k')
+%     plot(Amp(:,d),'-r','linewidth',2)
+%   end
+% end
+
+% opts.numIts = 200; 
+% [X1Est,X2Est,Info] = mPAD_infer_X1X2(y,X2,Params,opts);
+
+% AEst = log(1+exp(X2Est(1:T,:)*G'+ones(T,1)*Mu2'));
+
+% if dispFigs==1
+
+%   figure
+%   plot(Info.Obj)
+  
+%   figure
+%   for d=1:D
+%     subplot(D,1,d)
+%     hold on
+%     YchanEst = real(X1Est).*AEst;
+    
+%     plot(YchanEst(:,d),'-k')
+%     plot(Amp(:,d),'-b')
+%     plot(AEst(:,d),'-r','linewidth',2)  
+%   end
+  
+% end
+
+% X2Error = sqrt(sum(sum((X2(1:T,:)-X2Est(1:T,:)).^2)))/sqrt(sum(sum(X2(1:T,:).^2)));
+
+% if dispFigs==1
+%   disp(['Envelopes have moved by ',num2str(round(X2Error*100)),'%'])
+% end
+
+% assertTrue(X2Error<0.2) % envelopes move by less than 20%
+% %tol = 1e-1;
+% %assertVectorsAlmostEqual(G,GEst,'absolute',tol,0)
+
+
+function test_initialised_at_random_X2_and_move_toward_true
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Forward model Settings
+dispFigs=1;
+
+opts.verbose = dispFigs;
+seed = 1; 
+randn('state',seed)
+rand('state',seed)
+
+T = 1000;
+FSamp = 8000;
+D = 3;
+K = 2;
+Dims = packDimsMPAD(D,K,T);
+
+% Carrier Processes
+mVar1 = ones(1,D);
+CF = [600,750,1000]/FSamp;
+DF = [60,100,200]/FSamp;
+
+% Modulator Processes
+Len2 = [20,50]; % time constant in samples of modulators
+Mu2 = -ones(D,1);
+Var2 = 9*ones(K,1);
+
+G = [1,0;
+     0,1;
+     1,1];
+
+vary = 0.001;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GENERATE DATA
+
+[om,Lam1,Var1] = freq2probSpec(CF,DF,mVar1);
+
+Params = packParamsMPAD(G,Lam1,Var1,Len2,Var2,Mu2,vary,om);
+[y,X1,X2,Amp] = sampleMPAD(Params,Dims);
+
+if dispFigs==1
+
+  figure
+  for d=1:D
+    subplot(D,1,d)
+    hold on
+    Ychan = real(X1).*Amp;
+    plot(Ychan(:,d),'-k')
+    plot(Amp(:,d),'-r','linewidth',2)
+  end
+end
+
+Z2Init = ones(size(X2))*0;
+X2Init = Z2_to_X2(Z2Init,Params.Len2,Params.Var2,7);
+opts.numIts = 200; 
+[X1Est,X2Est,Info] = mPAD_infer_X1X2_basis_func(y,Z2Init,Params,opts);
+
+AEst = log(1+exp(X2Est(1:T,:)*G'+ones(T,1)*Mu2'));
+
+if dispFigs==1
+
+  figure
+  plot(Info.Obj)
+  
+  figure
+  for d=1:D
+    subplot(D,1,d)
+    hold on
+    YchanEst = real(X1Est).*AEst;
+    
+    plot(YchanEst(:,d),'-k')
+    plot(Amp(:,d),'-b')
+    plot(AEst(:,d),'-r','linewidth',2)  
+  end
+  
+end
+
+X2Error1 = sqrt(sum(sum((X2(1:T,:)-X2Est(1:T,:)).^2)))/sqrt(sum(sum(X2Est(1:T,:).^2)));
+
+X2Error2 = sqrt(sum(sum((X2Init(1:T,:)-X2Est(1:T,:)).^2)))/sqrt(sum(sum(X2Est(1:T,:).^2)));
+
+X2Error0 = sqrt(sum(sum((X2Init(1:T,:)-X2(1:T,:)).^2)))/sqrt(sum(sum(X2Est(1:T,:).^2)));
+
+
+if dispFigs==1
+  disp(['Envelopes have moved by ',num2str(round(X2Error2*100)),'%'])
+  disp(['Error on initialisation ',num2str(round(X2Error0*100)),'%'])
+  disp(['Error after training ',num2str(round(X2Error1*100)),'%'])
+end
+
+assertTrue(X2Error1<X2Error0) % envelopes move closer to the true
+                              % values - not a very strong test 
+
+
