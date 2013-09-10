5c2c67c5caec45cfd016cd11816bb487df0cf295
Richard E. Turner <richard.e.turner@gmail.com>
2013-09-10 16:29:24 +0100

    Extended to handle vectorised inputs/outputs.

--- mPAD/toolsGP/basis2SEGP.m
+++ mPAD/toolsGP/basis2SEGP.m
@@ -1,23 +1,28 @@
-function y = basis2SEGP(z,lenx,varx,tol)
+function Y = basis2SEGP(Z,lenx,varx,tol)
 
-% function y = basis2SEGP(z,lenx,varx,tol)
+% function Y = basis2SEGP(Z,lenx,varx,tol)
 %
 % converts the basis function representation of a squared
 % exponential Gaussian Process to the GP itself
 %
 % INPUTS
-% z = basis function coefficients [T,1]
-% lenx = horizontal length-scale parameter of the GP
-% varx = vertical scale parameter of GP (marginal variance)
+% Z = basis function coefficients [T,K]
+% lenx = horizontal length-scale parameter of the GP [K,1]
+% varx = vertical scale parameter of GP (marginal variance) [K,1]
 % tol = tolerence that sets size of the basis functions (probably
 %       should be > 9)
 %
 % OUTPUTS
-% y = Gaussian process
+% Y = Gaussian process [T,K]
 
-tau = ceil(lenx/sqrt(2)*tol);
-bh = sqrt(varx*sqrt(2)/(lenx*sqrt(pi))); % basis function height to
-                                         % get correct variance
-bas = bh*exp(-1/(lenx^2)*([-tau:1:tau]').^2);
-  
-y =  conv(z,bas,'same');
+K = length(lenx);
+Y = zeros(T,K);
+
+for k=1:K
+  tau = ceil(lenx(k)/sqrt(2)*tol);
+  bh = sqrt(varx(k)*sqrt(2)/(lenx(k)*sqrt(pi))); % basis function height to
+	          				 % get correct variance
+  bas = bh*exp(-1/(lenx(k)^2)*([-tau:1:tau]').^2);
+					   
+  Y(:,k) =  conv(Z(:,k),bas,'same');
+end
